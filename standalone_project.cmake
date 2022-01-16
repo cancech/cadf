7fc657d24f9c8e7aade272814e610767be49e450
Jaroslav Bosak <cancech@nowhere.com>
2022-01-16 12:46:46 -0500

    Removing standalone/nested build support.

--- standalone_project.cmake
+++
@@ -1,7 +0,0 @@
-# To be included in a project contained within cadf, to ensure that the project
-# can be handled independently of the rest of the cadf, and avoid issues with
-# infinite regression
-if(NOT DEFINED FULL_BUILD)
-    set(PROJECT_BUILD true)
-    add_subdirectory(".." build)
-endif()
